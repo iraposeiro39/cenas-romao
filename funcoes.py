789d17a70f3007f08feabdb178f82c323735cfe1
iraposeiro39 <daniel29.maia@gmail.com>
2025-05-30 21:55:41 +0100

    asdasdasd

--- funcoes.py
+++ funcoes.py
@@ -1,1 +1,23 @@
 #!/bin/python3
+
+## Importa todas as funções
+from tretas import *
+
+## Importa apenas uma função
+# from tretas import funcao1
+
+v = 100
+
+def funcao1(arg1):
+    valor_modificado = arg1 * 2
+    valor_original = arg1
+
+    return valor_modificado, valor_original
+    pass
+
+modificado, original = funcao1(v)
+
+print(f"Modificado: {modificado}")
+print(f"Original: {original}")
+
+funcao2()
---
+++ tretas.py
@@ -0,0 +1,15 @@
+#!/bin/python3
+
+def funcao1():
+    print("Acabaste de chamar a função numero 1!")
+    pass
+
+
+def funcao2():
+    print("Acabaste de chamar a função numero 2!")
+    pass
+
+
+def funcao3():
+    print("Acabaste de chamar a função numero 3!")
+    pass
